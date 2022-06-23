3c90fe8c87fe84923f5ae498b6f311b229e93925
Ahmet <dede_dede_ahmet@hotmail.com>
2022-06-24 02:16:49 +0300

    added toastification

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -4,8 +4,9 @@
       <h1>todo list</h1>
     </div>
     <div class="top-section" style="">
-      <input type="text" placeholder="Enter a task" v-model="task" v-on:keyup.enter="addToList()" style="padding: 5px">
-      <input type="submit" value="Add to List" class="btn btn-success" @click="submitTask">
+      <input type="text" placeholder="Enter a task" v-model="task" v-on:keyup.enter="submitTask()" style="padding: 6px">
+      <input type="submit" value="Add to List" class="btn btn-success" @click="submitTask()" style="margin-top: -7px">
+
     </div>
     <div>
       <table class="table mb-4">
@@ -23,8 +24,9 @@
           <td>{{ task.name }}</td>
           <td @click="changeStatus(index)"  style="width: 120px; cursor: pointer; user-select: none;">{{ task.status }}</td>
           <td>
+            <button type="submit" class="btn btn-success" @click="finishTask(index)">Complete</button>
+            <button type="submit" class="btn btn-warning" @click="editTask(index)">Edit</button>
             <button type="submit" class="btn btn-danger" @click="deleteTask(index)">Delete</button>
-            <button type="submit" class="btn btn-success ms-1" @click="finishTask(index)">Finished</button>
           </td>
         </tr>
         </tbody>
@@ -37,11 +39,11 @@
 
 <script>
 export default {
+
   data() {
     return {
       task: '',
       allStatus: ["no action", "in progress", "complete"],
-      msg: "errorr!",
       tasks: [
         {
           name: "wake-up",
@@ -56,23 +58,19 @@ export default {
   },
   methods: {
     submitTask() {
-      if(this.task === '') return;
+      if(this.task === '' || this.task.length < 3) return this.$toast.error("You must enter 3 characters at least!");
       this.tasks.push({
         name: this.task,
         status: "no action"
       })
-      this.task = ''
-    },
-    addToList(){
-      if(this.task === '') return;
-      this.tasks.push({
-        name: this.task,
-        status: "no action"
-      })
+      this.$toast.success("Added Successfully!")
       this.task = ''
     },
     deleteTask(index){
       this.tasks.splice(index,1)
+    },
+    editTask(index){
+
     },
     finishTask(index){
       this.tasks[index].status = "complete"
@@ -91,6 +89,10 @@ export default {
   border: 1px solid black;
   flex-direction: column;
   padding: 10px;
+}
+
+.btn{
+  width: 100px;
 }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
+const options = {
+  // You can set your default options here
+};
+Vue.use(Toast, options);
 
 new Vue({
   el: '#app',
